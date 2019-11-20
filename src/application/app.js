3eb9386ebed9c55a9ef6852089e56cbf8c85711d
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-11-20 12:17:08 -0300

    Fix awilix.loadModules call

--- src/application/app.js
+++ src/application/app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const awilix = require('awilix');
 const { scopePerRequest } = require('awilix-express')
 
-const container = awilix
-	.createContainer()
-	.loadModules([ '../domain/**/*.js' ], { formatName: 'camelCase' });
+const container = awilix.createContainer()
+
+container.loadModules(['../domain/**/*.js'], {
+	formatName: 'camelCase',
+	cwd: __dirname,
+	resolverOptions: {
+		lifetime: awilix.Lifetime.SCOPED,
+		register: awilix.asClass
+	}
+});
 
 const application = express();
 
@@ -14,6 +21,3 @@ application.use('/resource', require('./resource/resourceRoutes'));
 application.listen(4040, () => {
 	console.log(`Successfully listening on port 4040.`);
 });
-
-
-
